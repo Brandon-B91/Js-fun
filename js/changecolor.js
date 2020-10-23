994274308501846330d7abf357cc02372bac60b1
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2020-10-22 19:13:21 -0600

    added new project

--- js/changecolor.js
+++ js/changecolor.js
@@ -1,6 +1,6 @@
 let body = document.querySelector('body');
 let btn = document.querySelector('button');
-let colors = ['red', 'blue', 'green', 'purple', 'orange', 'green']
+let colors = ['red', 'blue', 'green', 'purple', 'orange', 'green', ]
 
 btn.addEventListener('click', () => {
     const colorIndex = Math.floor(Math.random() * 6)
